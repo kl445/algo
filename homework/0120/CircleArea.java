011ca0feb0d86f9af3305734179a1be195cb99b2
김동현 <kl445@naver.com>
2020-01-21 05:44:23 +0000

    Upload New File

---
+++ homework/0120/CircleArea.java
@@ -0,0 +1,13 @@
+package com.java.first;
+
+public class CircleArea {
+
+	public static void main(String[] args) {
+		int rad=5;
+		double area;
+		area=rad*rad*3.14;
+		System.out.print("반지름 "+rad+"Cm인 원의 넓이는 "+area+"Cm2 입니다.");
+
+	}
+
+}
